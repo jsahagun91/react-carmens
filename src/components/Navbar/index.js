804a2449c7c4cc5295fb9b10db054eea97db529c
Jose Sahagun <34168252+jsahagun91@users.noreply.github.com>
2024-04-26 09:00:27 -0700

    carmens logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,7 @@ const Navbar = ({ toggle }) => {
     return (
         <>
             <Nav>
-                <NavLink to='/'><img src={logo} alt="Logo" style={{ height: '50px', boxShadow:'rgba(0, 0, 0, 0.35) 0px 5px 15px' }}/></NavLink>
+                <NavLink to='/'><img src={logo} alt="Logo" style={{ height: '50px'}}/></NavLink>
                 {/* <NavIcon onClick={ toggle }>
                     <p>Menu</p>
                     <Bars />
